b5abaf0d418ea2df80cef73db2a42130cfa6a166
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-16 19:05:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  sentence_to_arr = sentence.split(/ /) #turns the string into an array 
+  sentence_to_arr = sentence.split(' ') #turns the string into an array 
   
   sentence_to_arr.collect do |word|
     word.reverse 
