d47bac33d5a2709c87685b8839b0ae75845a9bca
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-16 19:05:04 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
-# def reverse_each_word(sentence)
-#   sentence_to_arr = sentence.split(",") #turns the string into an array 
+def reverse_each_word(sentence)
+  sentence_to_arr = sentence.split(/ /) #turns the string into an array 
   
-#   sentence_to_arr.collect do |word|
-#     word.reverse 
-#   end
+  sentence_to_arr.collect do |word|
+    word.reverse 
+  end
   
-#   return reversed_words.join(",")
-# end
+  return reversed_words.join(",")
+end
 
 
 sentence = "Hello there, and how are you?"
